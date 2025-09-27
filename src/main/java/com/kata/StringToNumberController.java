7a6c6c3a89f769cd28b0e5e777bff5a859ba25b5
nabil aici <cybernaboo@yahoo.fr>
2025-09-28 00:35:29 +0200

    update 28/09/2025

---
+++ src/main/java/com/kata/StringToNumberController.java
@@ -0,0 +1,28 @@
+package com.kata;
+
+import com.kata.service.NumberToStringService;
+import com.kata.validation.NumberValidator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StringToNumberController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello Naboo Kata!";
+    }
+
+    @GetMapping("/numbertostring")
+    public String buildString(@RequestParam("inputvalue") String inputString) {
+        NumberToStringService numberToStringService = new NumberToStringService();
+        NumberValidator numberValidator = new NumberValidator();
+        String result = "";
+        if (numberValidator.isCorrectNumberValue(inputString)){
+            result = numberToStringService.convertNumberToString(Integer.parseInt(inputString));
+            return result;
+        }else{
+            return "\""+inputString+"\"";
+        }
+    }
+}
